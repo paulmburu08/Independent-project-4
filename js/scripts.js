735a93feec26d69cbff80c5efe00c23bad64d7ec
paulmburu08 <paulmburu08@gmail.com>
2020-04-20 00:09:05 +0300

    add changes

--- js/scripts.js
+++ js/scripts.js
@@ -12,25 +12,14 @@ $(document).ready(function(){
         var pizzaNumber = $("input[name='number']").val();
         //end//
         var pizzaSize = {
-            smallPizza : 600+"KES",
-            mediumPizza : 800+"KES",
-            largePizza : 1000+"KES",
+            smallPizza : 600,
+            mediumPizza : 800,
+            largePizza : 1000,
         };     
         var pizzaPrice = (pizzaSize[size])
-
-        function ToppingsPrice(first,second,third,fourth,fiveth,sixth){
-            this.pepperoni = first;
-            this.mushrooms = second;
-            this.onions = third;
-            this.bacon = fourth;
-            this.extraCheese = fiveth;
-            this.pineapple = sixth;
-        }
-        var smallPizza = new ToppingsPrice(100+"KES",100+"KES",100+"KES",100+"KES",100+"KES",100+"KES");
-        var mediumPizza = new ToppingsPrice(150+"KES",150+"KES",150+"KES",150+"KES",150+"KES",150+"KES");
-        var largePizza = new ToppingsPrice(200+"KES",200+"KES",200+"KES",200+"KES",200+"KES",200+"KES");
-
-        alert((size).pineapple)
+        var toppingsPrice = toppings.length * 200
+        var onePizza = pizzaPrice + toppingsPrice
+        alert(onePizza)
 
     });
 
