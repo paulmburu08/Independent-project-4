6c1ca1855e1f6eb144e3fba7b0a0287fc36ba811
paulmburu08 <paulmburu08@gmail.com>
2020-04-20 01:14:58 +0300

    add changes

--- js/scripts.js
+++ js/scripts.js
@@ -23,19 +23,33 @@ $(document).ready(function(){
     
         if(pizzaNumber>1){
             var totalPrice1 = onePizza * pizzaNumber
-            alert('Your total Order is '+ totalPrice1 +'KES. Please choose below if you would like your order delivered.')
+            alert('Your total Order is '+ totalPrice1 +'KES. Please choose below if you would like your order delivered i.e Extra charges include')
         }  
         else if(pizzaNumber==1){
-            var totalPrice1 = onePizza
-            alert('Your total Order is '+ totalPrice1 +'KES. Please choose below if you would like your order delivered.')
+            var totalPrice2 = onePizza
+            alert('Your total Order is '+ totalPrice2 +'KES. Please choose below if you would like your order delivered i.e Extra charges include')
         }
         else{
             alert('Please select number of pizzas you want to order')
         }
-       
-    
 
     });
+       
+    $("#yesDelivery").click(function(event){
+        event.preventDefault();
+
+        var location = prompt('Please enter location')
+        var confirmation = confirm("Delivery to your location costs 400KES.Continue?")
+        if(confirmation===true){
+            alert("Thank you. Your order will be delivered to your location")
+        }
+        else{
+            alert("You have cancelled delivery.")
+        }
+    });
+        
+
+    
 
 
 
