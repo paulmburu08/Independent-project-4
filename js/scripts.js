99e57977024058bdd50c535ba6f4dfd00cb894da
paulmburu08 <paulmburu08@gmail.com>
2020-04-19 17:36:02 +0300

    add changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,12 @@
 $(document).ready(function(){
 
+    $("#checkOut").click(function(){
+        $("#submit").click(function(){
+            var pizzaSize = $("input[name='size']:checked").val();
+            
+            alert(pizzaSize)
+        });
+
+    });
 
 });
