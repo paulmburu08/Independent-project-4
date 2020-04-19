a6d1548fb782f088d9a4f1b1b7fe287359ac9ca5
paulmburu08 <paulmburu08@gmail.com>
2020-04-19 22:23:24 +0300

    add delivery buttons

--- js/scripts.js
+++ js/scripts.js
@@ -2,15 +2,36 @@ $(document).ready(function(){
 
     
     $("#submit").click(function(){
+        //getting user input//
         var size = $("input[name='size']:checked").val();
         var crust = $("input[name='crust']:checked").val();
         var toppings = [];
         $.each($("input[name='toppings']:checked"), function(){
             toppings.push($(this).val());
         });
-        console.log(toppings)
-        
-        
+        var pizzaNumber = $("input[name='number']").val();
+        //end//
+        var pizzaSize = {
+            small : 600+"KES",
+            medium : 800+"KES",
+            large : 1000+"KES",
+        };     
+        var pizzaPrice = (pizzaSize[size])
+
+        function ToppingsPrice(first,second,third,fourth,fiveth,sixth){
+            this.pepperoni = first;
+            this.mushrooms = second;
+            this.onions = third;
+            this.bacon = fourth;
+            this.extraCheese = fiveth;
+            this.pineapple = sixth;
+        }
+        var smallPizza = new ToppingsPrice(100+"KES",100+"KES",100+"KES",100+"KES",100+"KES",100+"KES");
+        var mediumPizza = new ToppingsPrice(150+"KES",150+"KES",150+"KES",150+"KES",150+"KES",150+"KES");
+        var largePizza = new ToppingsPrice(200+"KES",200+"KES",200+"KES",200+"KES",200+"KES",200+"KES");
+
+        alert(largePizza.pepperoni);
+
     });
 
 
