62c5bd0ac0c64bbb1ec6da44ee8a1e3de08cfb36
paulmburu08 <paulmburu08@gmail.com>
2020-04-20 02:34:28 +0300

    add changes

--- js/scripts.js
+++ js/scripts.js
@@ -26,31 +26,52 @@ $(document).ready(function(){
             alert('Your total Order is '+ totalPrice1 +'KES. Please choose below if you would like your order delivered i.e Extra charges include')
         }  
         else if(pizzaNumber==1){
-            var totalPrice2 = onePizza
-            alert('Your total Order is '+ totalPrice2 +'KES. Please choose below if you would like your order delivered i.e Extra charges include')
+            var totalPrice1 = onePizza
+            alert('Your total Order is '+ totalPrice1 +'KES. Please choose below if you would like your order delivered i.e Extra charges include')
         }
         else{
             alert('Please select number of pizzas you want to order')
         }
 
-    });
-       
+    
+        var flag = 0;
     $("#yesDelivery").click(function(event){
         event.preventDefault();
-
+        flag = 1;
         var location = prompt('Please enter location')
         var confirmation = confirm("Delivery to your location costs 400KES.Continue?")
         if(confirmation===true){
-            alert("Thank you. Your order will be delivered to your location")
+            alert("Thank you. Your order will be delivered to your location. Please proceed to checkout to process your order")
         }
         else{
             alert("You have cancelled delivery.")
         }
-    });
         
+    });
 
+    var yes = $("#noDelivery").click(function(event){
+        event.preventDefault();
+        hasBeenClicked = true;
+        alert("Please proceed to checkout to process your order")
+
+    });
     
+    $("#checkOut").click(function(event){
+        event.preventDefault();
+        var totalPrice2 = parseInt(totalPrice1);
+        var totalPrice3 = totalPrice2 + 400;
+
+        if(flag==1){
+        $("#cart").append("<b>TOTAL COST</b><br>Pizza price: "+ totalPrice1 + "KES<br>Delivery: 400KES<br><b>GRAND TOTAL</b>: "+ totalPrice3 +"KES.")
+        alert("Your delivery is on it's way. Enjoy your meal!!")
+        }
+
+        else{
+            $("#cart").append("<b>TOTAL COST</b><br>Pizza price: "+ totalPrice1 + "KES<br>Delivery: None<br><b>GRAND TOTAL</b>: "+ totalPrice2 +"KES.") 
+        }
+    });    
 
 
+});
 
 });
